3d8540f8d50d7f7c0a40450bbfceb6b9b9ea2d28
ROY <roy@gmail.com>
2024-07-14 14:45:50 +0530

    First commit

---
+++ src/test/java/com/example/SpringbootWorkspace/SpringbootWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootWorkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
